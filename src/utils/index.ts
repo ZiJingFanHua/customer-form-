67daee40208b7ca1b66b71749cd36920b583c72e
刘家豪 <zijingfanhua@163.com>
2024-04-10 17:28:45 +0800

    feat:深克隆方法

---
+++ src/utils/index.ts
@@ -0,0 +1,34 @@
+//深拷贝对象
+export function deepClone(obj) {
+  const _toString = Object.prototype.toString;
+
+  //null undefined function non-object
+  if (!obj || typeof obj! == 'object') {
+    return obj
+  }
+
+  // Dom Node
+  if (obj.nodeType && 'clonenode' in obj) {
+    return obj.clonenode(true)
+  }
+
+  //Date
+  if (_toString.call(obj) === '[object Date]') {
+    return new Date(obj.getTime())
+  }
+
+  if (_toString.call(obj) === '[object RegExp]') {
+    const flags = [] as any
+    if (obj.global) { flags.push('g') }
+    if (obj.multiline) { flags.push('m') }
+    if (obj.ignoreCase) { flags.push('i') }
+    return new RegExp(obj.source, flags.join(''))
+  }
+
+  const result = Array.isArray(obj) ? [] : obj.constructor ? new obj.constructor : {}
+
+  for (const key in obj) {
+    result[key] = deepClone(obj[key])
+  }
+  return result
+}
